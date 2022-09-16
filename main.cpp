0268d7674cc4211dd0fbc110e17b367c48839f6c
scorpiotea <112612010+scorpiotea@users.noreply.github.com>
2022-09-16 21:30:09 +0200

    A lot of changes

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 #include <cstdlib>
+
 using namespace std;
 
-int dice()
-{
-  int a;
-  a = rand() % 6 + 1;
-  return a;
-}
-int result()
-{
-  int result;
-  result = dice() + dice();
-  return result;
-}
 void rules()
 {
   cout << " " << endl
@@ -26,24 +15,27 @@ void rules()
        << "numbers 7 or 11 ( you lose ) or the equivalent \n of your starting number ( you win )." << endl
        << " " << endl;
 }
-class Player
-{
-public:
-  string username;
 
-  void getUsername()
-  {
-    cout << "Enter username: ";
-    cin >> username;
-  }
-};
+int throwDice()
+{
+  int a;
+  a = rand() % 6 + 1;
+  return a;
+}
+
+int result()
+{
+  int result;
+  result = throwDice() + throwDice();
+  return result;
+}
 
 int main()
 {
-  int user_number, user_number2, end, mode, score, score2;
-  string choice;
+  int userNumber2, end, score, score2, mode, endGame;
+  string choice, playerName;
 
-  Player one;
+  int playerNumber = throwDice() + throwDice();
 
   cout << "MENU :" << endl
        << "Singleplayer mode (1)" << endl
@@ -51,141 +43,52 @@ int main()
        << " " << endl;
   cout << "Choose the mode you want to play: ";
   cin >> mode;
-
-  if (mode == 1)
-  {
-    rules();
-    one.getUsername();
-  }
-
-  if (mode == 2)
-  {
-    rules();
-
-    // cout << "Player one name: ";
-    // cin >> user_name;
-    // cout << "Player two name: ";
-    // cin >> user_name2;
-  }
+  cout << " " << endl
+       << "START" << endl;
+  rules();
+  cout << "Enter username: ";
+  cin >> playerName;
 
   do
   {
-
-    cout << " " << endl
-         << "START" << endl
-         << " " << endl;
-
-    user_number = dice() + dice();
-
     if (mode == 1)
     {
-      cout << " " << one.username << " your number is " << user_number << endl;
-    }
+      int playerNumber = throwDice() + throwDice();
 
-    if (user_number == 7 || user_number == 11)
-    {
-      cout << " " << one.username << " you won :)" << endl;
-      end = 1;
-    }
-    if (user_number == 2)
-    {
-      cout << " " << one.username<< " you lost :(" << endl;
-      end = 1;
-    }
-    if (mode == 2)
-    {
-      user_number2 = dice() + dice();
-      cout << " " << one.username << " your number is " << user_number << endl;
-      cout << " " << one.username << " your number is " << user_number2 << endl;
-      if (user_number == 7 || user_number == 11)
+      cout << playerName << " your number is " << playerNumber << endl;
+
+      if (playerNumber == 7 || playerNumber == 11)
       {
-        cout << " " << one.username << " won :)" << endl;
-        cout << " " << one.username << " lost :(" << endl;
-        end = 1;
+        cout << playerName << " you won :)";
+        endGame = 1;
       }
-      if (user_number == 2)
+      if (playerNumber == 2)
       {
-        cout << " " << one.username << " lost :(" << endl;
-        cout << " " << one.username << " won :)" << endl;
-        end = 1;
-      }
-      if (user_number2 == 7 || user_number2 == 11)
-      {
-        cout << " " << one.username << " won :)" << endl;
-        cout << " " << one.username << " lost :(" << endl;
-        end = 1;
-      }
-      if (user_number2 == 2)
-      {
-        cout << " " << one.username << " lost :(" << endl;
-        cout << " " << one.username << " won :)" << endl;
-        end = 1;
+        cout << playerName << " you lost :(";
+        endGame = 1;
       }
       while (end != 1)
       {
         score = result();
-        score2 = result();
-        cout << " " << one.username << " threw out " << score << endl;
-        cout << " " << one.username << " threw out " << score2 << endl;
+        cout << " " << playerName << " threw out " << score << endl;
 
         if (score == 7 || score == 11)
         {
-          cout << " " << one.username << " lost :(" << endl;
-          cout << " " << one.username << " won :)" << endl;
-          end = 1;
+          cout << " " << playerName << " you lost :(" << endl;
+          endGame = 1;
         }
-        if (score2 == 7 || score2 == 11)
-        {
-          cout << " " << one.username << " lost :(" << endl;
-          cout << " " << one.username << " won :)" << endl;
-          end = 1;
-        }
-        if (end == 1)
+        if (endGame == 1)
         {
           continue;
         }
-        if (score == user_number)
+        if (score == playerNumber)
         {
-          cout << " " << one.username << " won :)" << endl;
-          cout << " " << one.username << " lost :(" << endl;
-          end = 1;
-        }
-        if (score2 == user_number2)
-        {
-          cout << " " << one.username << " won :)" << endl;
-          cout << " " << one.username << " lost :(" << endl;
-          end = 1;
+          cout << " " << playerName << " you won :)" << endl;
+          endGame = 1;
         }
       }
     }
-    while (end != 1)
-    {
-      score = result();
-      cout << " " << one.username << " threw out " << score << endl;
-      if (score == 7 || score == 11)
-      {
-        cout << " " << one.username << " you lost :(" << endl
-             << " " << endl;
-        end = 1;
-      }
-      if (end == 1)
-      {
-        continue;
-      }
-      if (score == user_number)
-      {
-        cout << " " << one.username << " you won :)" << endl
-             << " " << endl;
-        end = 1;
-      }
-    }
-
-    cout << " Do you want to play again? ( yes or no ) " << endl;
-    cout << " Your choice is : ";
-    cin >> choice;
-    end = 0;
-
   } while (choice == "yes");
-
+  
   return 0;
 }
